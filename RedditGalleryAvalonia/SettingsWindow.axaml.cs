085a037b5ef43245cace886ce457ec7d260af7ab
Marcel Hasse <marcel.hasse@outlook.de>
2022-09-28 22:16:19 +0200

    Implemented changeable subreddit; started on settings window

---
+++ RedditGalleryAvalonia/SettingsWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaAppTemplate.Namespace
+{
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
